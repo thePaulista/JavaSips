9c42a47f63b3e883020cc63371762451c5afdffd
Hedy <hedywoo@gmail.com>
2016-07-15 22:30:56 -0600

    Search target loop judo

---
+++ StringKata/src/practice/loopchallenge/TargetSearch.java
@@ -0,0 +1,25 @@
+package practice.loopchallenge;
+
+public class TargetSearch {
+	public static void main(String[] args) {
+		System.out.println(search(new int[] {2,3,4,5,6,7}, 5)); // 3
+		System.out.println(search(new int[] {2,3,4,5,6,7}, 9)); // -1
+		
+		}
+	
+	public static int search(int[] nums, int target) {
+		
+		int ret = -1;
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] == target) {
+				ret = i;
+				break;
+			} 
+			
+		}
+		
+		return ret;
+		
+	}
+
+}
